907ce03407e5f32ed0b4ae23aa4b9292084fdeb2
Hikmahx <hikmayousuph@gmail.com>
2026-04-19 17:01:09 +0100

    refactor: update Settings class attributes to uppercase for consistency

--- fastapi-backend/app/core/config.py
+++ fastapi-backend/app/core/config.py
@@ -6,34 +6,36 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 class Settings(BaseSettings):
-    # change all to lowercase
-    
-    app_name: str = "QuizMe API"
-    app_version: str = "1.0.0"
-    debug: bool = False
-    allowed_origins: list[str] = ["http://localhost:3000"]
-    database_url: str
-    groq_api_key: str
-    groq_model: str = "llama-3.3-70b-versatile"
-    groq_max_token: int = 2048
-    groq_temperature: float = 0.3
-    embedding_model: str = "all-MiniLM-L6-v2"
-    embedding_dimension: int = 384
-    chunk_size: int = 1000
-    chunk_overlap: int = 100
+
+    APP_NAME: str = "QuizMe API"
+    APP_VERSION: str = "1.0.0"
+    DEBUG: bool = False
+    ALLOWED_ORIGINS: list[str] = ["http://localhost:3000"]
+    DATABASE_URL: str
+
+    GROQ_API_KEY: str
+    GROQ_MODEL: str = "llama-3.3-70b-versatile"
+    GROQ_MAX_TOKENS: int = 2048
+    GROQ_TEMPERATURE: float = 0.3
+
+    EMBEDDING_MODEL: str = "all-MiniLM-L6-v2"
+    EMBEDDING_DIMENSION: int = 384
+
+    CHUNK_SIZE: int = 1000
+    CHUNK_OVERLAP: int = 100
 
     # How many chunks to return when searching (like SQL LIMIT)
-    retrieval_top_k: int = 5
+    RETRIEVAL_TOP_K: int = 5
     # For quiz generation we want broader coverage of the document
-    retrieval_top_k_broad: int = 15
+    RETRIEVAL_TOP_K_BROAD: int = 15
 
     # Chunks are deleted if not accessed for this many days
-    collection_ttl_days: int = 3
-    cleanup_interval_hours: int = 6
+    COLLECTION_TTL_DAYS: int = 3
+    CLEANUP_INTERVAL_HOURS: int = 6
 
-    max_file_size_mb: int = 20
-    max_files_per_session: int = 10
-    allowed_mime_types: list[str] = [
+    MAX_FILE_SIZE_MB: int = 20
+    MAX_FILES_PER_SESSION: int = 10
+    ALLOWED_MIME_TYPES: list[str] = [
         "application/pdf",
         "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
         "text/plain",
@@ -43,6 +45,8 @@ class Settings(BaseSettings):
     class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"
+        extra = "allow"
+
 
 @lru_cache
 def get_settings() -> Settings:
