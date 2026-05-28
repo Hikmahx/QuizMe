594d0990ebf818fb4ab475856e98d1a7df37b354
Hikmahx <hikmayousuph@gmail.com>
2026-05-28 22:21:37 +0100

    Add Babel and Metro configuration files for React Native project setup

---
+++ mobile-app/babel.config.js
@@ -0,0 +1,10 @@
+module.exports = (api) => {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      ['module-resolver', { alias: { '@': './' } }],
+      'react-native-reanimated/plugin',
+    ],
+  };
+};
